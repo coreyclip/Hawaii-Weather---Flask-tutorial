07dc4bc3efe90fddf0c90228fa199855d1537bbf
corey clippinger <coreyclip@gmail.com>
2018-04-11 18:17:11 -0700

    finished draft of flaskapp

--- Solution/flaskapp.py
+++ Solution/flaskapp.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify,render_template
 
 engine = create_engine("sqlite:///hawaii.sqlite")
 
@@ -32,49 +32,64 @@ app = Flask(__name__)
 # Define what to do when a user hits the index route
 @app.route("/")
 def home():
-    print("Server received request for 'Home' page...")
-    return (
-        f"Hawaii Weather Flask Tutorial:<br/>"
-        f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation")
-
-
+	print("Server received request for 'Home' page...")
+	return (
+	f"Hawaii Weather Flask Tutorial:<br/>"
+	f"Available Routes:<br/>"
+	f"/about:<br/>"
+	
+	f"/api/v1.0/precipitation:<br/>"
+	f"/api/v1.0/stations:<br/>")
+	
+	
 # 4. Define what to do when a user hits the /about route
 @app.route("/about")
 def about():
-    print("Server received request for 'About' page...")
-    return "This is a simple demostration of connecting a \n SQL lite database to a web app"
-
-
+	print("Server received request for 'About' page...")
+	return "This is a simple demostration of connecting a \n SQL lite database to a web app"
+	
+	
 @app.route("/api/v1.0/precipitation")
-"""
-  * Query for the dates and temperature observations from the last year.
-
-  * Convert the query results to a Dictionary using `date` as the key and `tobs` as the value.
-
-  * Return the json representation of your dictionary.
-"""
 def prcp(year):
-    results = session.query(measures.date, measures.prcp).\
-                filter(func.strftime("%y", measures.date) == year).all()
-    prcp_recs = []
-    for rec in results:
-        date_dict = {}
-        date_dict['date'] = rec.prcp
-        prcp_rec.append(date_dict)
-    return jsonify(prcp_recs)
-        
-    
+	results = session.query(measures.date, measures.prcp).\
+	filter(func.strftime("%y", measures.date) == year).all()
+	prcp_recs = []
+	for rec in results:
+		date_dict = {}
+		date_dict['date'] = rec.prcp
+		prcp_rec.append(date_dict)
+	return jsonify(prcp_recs)
+	
+	
 @app.route("/api/v1.0/stations")
-"""
+
+def stations():
+	"""
 * Return a json list of stations from the dataset.
 """
-
+	results = session.query(stations.name).all()
+	records = list()
+	for name in results:
+		records.append(name)
+		
+	return jsonify(records)
+	
 @app.route("/api/v1.0/tobs")
 """
 * Return a json list of Temperature Observations (tobs) for the previous year
 """
-
+def tobs(year):
+	results = session.query(measures.date, measures.tobs).\
+	filter(func.strftime("%y", measures.date) == year).all()
+	tobs_recs = []
+	for rec in results:
+		date_dict = {}
+		date_dict['date'] = rec.tobs
+		tobs_rec.append(date_dict)
+	return jsonify(tobs_recs)
+	
+	
+	
 @app.route("/api/v1.0/<start>")
 """
 * Return a json list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
@@ -83,8 +98,17 @@ def prcp(year):
 
   * When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
 """
-
-
+def calc_temp(start,end):
+	if end:
+		results = session.query(measures.date,            measures.tobs)./filter(func.strftime(measures.date) < end)./
+		filter(func.striftime(measures.date) > start).all()
+	else:
+		results = session.query(measures.date,            measures.tobs)./
+		filter(func.striftime(measures.date) > start).all()
+		
+		
+		
+		
 @app.route("/api/v1.0/<start>/<end>")
 """
 * Return a json list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
@@ -92,7 +116,8 @@ def prcp(year):
   * When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
 
   * When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
-"""    
+"""
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
+
